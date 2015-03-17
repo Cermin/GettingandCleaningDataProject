1db11c0da694383e2aa82dee9c7f7fe9c800a4b8
Usha Morris <UshaNMorris@gmail.com>
2015-03-17 13:03:01 -0400

    Added the set working directory
    Set my working directory

--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,42 @@
+setwd("/Users/Usha/Documents/UCI HAR Dataset")
 library(downloader)  ## used for downloading https files
 library(utils)       ## needed for unzip function
 
-########## Download the zipped file and the unzip into a folder called Dataset #################################
+
+########## Step 1: Download the zipped file and the unzip into a folder called Dataset #################################
 URL<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download(URL, dest="Dataset.zip", mode="wb")
 unzip ("Dataset.zip")
  
 ################################################################################################
 
-##  Step 1: Merges the training and the test sets to create one data set
+##  Step 2: Merges the training and the test sets to create one data set
 ##  Need to read the train and test data  (subject,X and Y)
 ##  There is a total of 6 files to read in and then merge.
 ##  Read the files individually and then do a cbind first for test and train separately.
 ##  Then rbind to to merge test and train data
 
-VarNames<-read.table("/Users/Usha/Documents/UCI HAR Dataset/features.txt")
+VarNames<-read.table("features.txt")
 
 ##  read the test data into a data frame and rename the columns per the values in VarNames
-x_test<-read.table("/Users/Usha/Documents/UCI HAR Dataset/test/X_test.txt")
+x_test<-read.table("./test/X_test.txt")
 colnames(x_test)<-VarNames[,2] 
 
-subject_test<-read.table("/Users/Usha/Documents/UCI HAR Dataset/test/subject_test.txt")
+subject_test<-read.table("./test/subject_test.txt")
 colnames(subject_test)<-"subject_ID"
 
-y_test<-read.table("/Users/Usha/Documents/UCI HAR Dataset/test/y_test.txt")
+y_test<-read.table("./test/y_test.txt")
 colnames(y_test)<- "activity_name"
 
 ##  read the train data into a data frame and rename the columns per the values in VarNames
 
-x_train<-read.table("/Users/Usha/Documents/UCI HAR Dataset/train/X_train.txt")
+x_train<-read.table("./train/X_train.txt")
 colnames(x_train)<-VarNames[,2]
 
-subject_train<-read.table("/Users/Usha/Documents/UCI HAR Dataset/train/subject_train.txt")
+subject_train<-read.table("./train/subject_train.txt")
 colnames(subject_train)<- "subject_ID"
 
-y_train<-read.table("/Users/Usha/Documents/UCI HAR Dataset/train/y_train.txt")
+y_train<-read.table("./train/y_train.txt")
 colnames(y_train)<- "activity_name"
 
 # fixed variables come before the measured variables
@@ -49,7 +51,7 @@ MergedData<- rbind(datatest,datatrain)
 dim(MergedData)
 head(MergedData)
 
-## Step 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+## Step 3: Extracts only the measurements on the mean and standard deviation for each measurement.
 
 data_meanextract<- MergedData[ , grepl( "*mean*" , names(MergedData) ) ]
 colnames(data_meanextract)
@@ -65,7 +67,7 @@ head(full_dataextract)
 dim(full_dataextract)
 
 
-## Step 3: Uses descriptive activity names to name the activities in the data set
+## Step 4: Uses descriptive activity names to name the activities in the data set
 ## In the activity_labels.txt there are 6 activity names that are used in the y test and train files.
    
 for (i in 1:10299){
@@ -90,7 +92,7 @@ for (i in 1:10299){
 }
 
 
-##  Step 4: Appropriately labels the data set with descriptive variable names
+##  Step 5: Appropriately labels the data set with descriptive variable names
 ##  I did not change the variables names ( they made sense to me) given in the features.txt file other than  
 ##  for the variables that had the word "Body" appearing twice in their names.
 ##  The variable names in features.txt was used in place of V1:V561 in step 1.
@@ -110,7 +112,7 @@ dim(sortedData)  # 10299 81
 
 
 
-##  Step 5:From the data set in step 4, create a second, independent tidy data set with
+##  Step 6:From the data set in step 4, create a second, independent tidy data set with
 ##   the average of each variable for each activity and each subject.
 ##   data set from step 4 is named sortedData
 
